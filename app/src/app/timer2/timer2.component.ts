b8a19515903187611e7d86d42700017ca6d081d9
AleksandrBracyuk <aleksandr.bracyuk@abcsolutions.pro>
2020-08-29 14:52:42 +0300

    работает поток событий (добавляется событие стоп) и переводится в состояние (без кол-ва, без обработки reset)

--- app/src/app/timer2/timer2.component.ts
+++ app/src/app/timer2/timer2.component.ts
@@ -20,14 +20,24 @@ import { buffer, filter, throttleTime } from 'rxjs/operators';
 
 enum Timer2ClickButton {
   startButton,
-  // stopButton,
   waitButton,
   resetButton,
+  stopButton,
 }
 class Timer2State {
+  isStarted: boolean = false;
+  isWaited: boolean = false;
+  currentSecond: number = 0;
+}
+interface Time2AccumulateState {
   isStarted: boolean;
   isWaited: boolean;
   currentSecond: number;
+}
+interface Time2StateCommand {
+  isStarted: boolean;
+  isWaited: boolean;
+  command: Timer2ClickButton;
 }
 
 @Component({
@@ -41,7 +51,7 @@ export class Timer2Component implements OnInit, AfterViewInit {
   @ViewChild('resetButton') resetButton: ElementRef;
 
   data: Observable<Date>;
-  currentState: Timer2State;
+  currentState: Timer2State = new Timer2State();
 
   constructor() {}
 
@@ -59,46 +69,91 @@ export class Timer2Component implements OnInit, AfterViewInit {
     //   buffer(waitButtonStreamRaw$.pipe(throttleTime(300))),
     //   filter((clickArray) => clickArray.length > 1)
     // );
-    let events$ = merge(
+    let eventsRam$ = merge(
       stream(this.startButton.nativeElement, Timer2ClickButton.startButton),
       stream(this.waitButton.nativeElement, Timer2ClickButton.waitButton),
       // waitButtonStream$,
       stream(this.resetButton.nativeElement, Timer2ClickButton.resetButton)
     );
 
-    let super$ = events$.pipe(
-      startWith(new Date(2020, 0, 1)),
-      switchMap((e) => {
-        this.currentState = this.NextState(this.currentState, e);
-        return this.SwitchMap(this.currentState);
-      })
+    let events$ = eventsRam$.pipe(
+      map((x) => ({ isStarted: false, isWaited: false, command: x })),
+      scan(
+        (s: Time2StateCommand, curr) => ({
+          ...s,
+          ...{
+            isStarted:
+              curr.command == Timer2ClickButton.startButton && !s.isStarted
+                ? true
+                : curr.command == Timer2ClickButton.startButton &&
+                  s.isStarted &&
+                  !s.isWaited
+                ? false
+                : s.isStarted,
+          },
+          ...{
+            isWaited:
+              curr.command == Timer2ClickButton.waitButton
+                ? true
+                : curr.command == Timer2ClickButton.startButton
+                ? false
+                : s.isWaited,
+          },
+          ...{
+            command:
+              s.command == Timer2ClickButton.stopButton
+                ? curr.command
+                : curr.command == Timer2ClickButton.startButton &&
+                  s.isStarted &&
+                  !s.isWaited
+                ? Timer2ClickButton.stopButton
+                : curr.command,
+          },
+        }),
+        {
+          isStarted: false,
+          isWaited: false,
+          command: Timer2ClickButton.stopButton,
+        }
+      )
     );
 
-    this.data = super$;
+    // let state$ = events$.pipe(
+    //   map(x=>this.NextState(x,e))
+    // );
 
-    super$.subscribe((x) => {
-      console.log(x.toTimeString().substr(0, 8));
-    });
+    // let super$ = events$.pipe(
+    //   startWith(new Date(2020, 0, 1)),
+    //   switchMap((e) => this.SwitchMap(e))
+    // );
+
+    // this.data = super$;
+
+    // super$.subscribe((x) => {
+    //   console.log(x.toTimeString().substr(0, 8));
+    // });
     events$.subscribe((x) => {
-      console.log('events:' + x);
+      console.log('events', x);
     });
   }
-  private SwitchMap(currentState: Timer2State): Observable<Date> {
-    let run$ = interval(1000).pipe(
-      map((x) => new Date(2020, 0, 1, 0, 0, currentState.currentSecond))
-    );
+
+  private SwitchMap(command: Timer2ClickButton): Observable<Date> {
+    this.currentState = this.NextState(this.currentState, command);
+    let run$ = interval(1000).pipe(map((x) => new Date(2020, 0, 1, 0, 0, x)));
     let wait$ = NEVER.pipe(
-      startWith(new Date(2020, 0, 1, 0, 0, currentState.currentSecond))
+      startWith(new Date(2020, 0, 1, 0, 0, this.currentState.currentSecond))
     );
-    if (currentState.isWaited) {
+    console.log(this.currentState);
+    if (this.currentState.isWaited) {
       return wait$;
     } else {
-      if (currentState.isStarted) {
+      if (this.currentState.isStarted) {
         return run$;
       } else {
         return wait$;
       }
     }
+    return wait$;
   }
 
   private NextState(
@@ -106,24 +161,22 @@ export class Timer2Component implements OnInit, AfterViewInit {
     command: Timer2ClickButton
   ): Timer2State {
     var nextState = currentState;
-
     switch (command) {
       case Timer2ClickButton.startButton:
         /*сняли с паузы*/
         if (currentState.isWaited) {
           nextState.isWaited = false;
-        }
-        /*остановили*/
-        if (currentState.isStarted) {
-          nextState.isStarted = false;
-          nextState.currentSecond = 0;
-        }
-        /*запустили*/
-        if (!currentState.isStarted) {
-          nextState.isStarted = true;
+        } else {
+          /*остановили*/
+          if (currentState.isStarted) {
+            nextState.isStarted = false;
+            nextState.currentSecond = 0;
+          } else {
+            /*запустили*/
+            nextState.isStarted = true;
+          }
         }
         break;
-
       case Timer2ClickButton.waitButton:
         /*поставили на паузу*/
         if (currentState.isStarted) {
@@ -141,5 +194,19 @@ export class Timer2Component implements OnInit, AfterViewInit {
     }
 
     return nextState;
+  }
+
+  public testSpreadInObject() {
+    let obj1 = {
+      isStarted: false,
+      isWaited: true,
+      currentSecond: 111,
+    };
+    let obj2 = {
+      isWaited: false,
+      currentSecond: 5,
+    };
+    let obj3 = { ...obj1, ...obj2 };
+    console.log(obj3);
   }
 }
